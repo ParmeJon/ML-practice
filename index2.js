4048aaa6892d7ecbc8c066b7810057f00aa344d5
github username <jonathansychan@gmail.com>
2019-10-22 15:56:30 -0400

    Applying the trainedNet

--- index2.js
+++ index2.js
@@ -1,13 +1,16 @@
 const brain = require("brain.js");
 const fs = require("fs");
 const { trainingData3 } = require("./data.js");
+const trainedNet = require("./flirty-trained-thirdNet.js");
 
-let thirdNet = new brain.recurrent.LSTM();
-thirdNet.train(trainingData3, {log: true});
+// let thirdNet = new brain.recurrent.LSTM();
+// thirdNet.train(trainingData3, {log: true});
 
-fs.writeFileSync(
-  "flirty-trained-thirdNet.js",
-  `module.exports = ${thirdNet.toFunction().toString()}`
-);
+// fs.writeFileSync(
+//   "flirty-trained-thirdNet.js",
+//   `module.exports = ${thirdNet.toFunction().toString()}`
+// );
 
-console.log(thirdNet.run("I like you"));
+// console.log(thirdNet.run("I like you"));
+
+console.log(trainedNet("i like you"))
